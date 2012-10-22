f24e1214b39263bf590ac589fbda4fdf65411b97
Gary Hogarth <gary.hogarth@hogarthww.com>
2012-10-22 17:49:28 +0100

    More changes
    Signed-off-by: Gary Hogarth <gary.hogarth@hogarthww.com>

--- src/Aoceu/ToolsBundle/Command/ScrapeCommand.php
+++ src/Aoceu/ToolsBundle/Command/ScrapeCommand.php
@@ -28,6 +28,23 @@ class ScrapeCommand extends ContainerAwareCommand {
         
         $output->writeln('Importing');
         
+        $client = $this->getHelperSet()->get('goutte')
+            ->getNamedClient('curl');
+
+        $crawler = $client->request('GET', 'http://www.spanishdict.com/conjugate/comer');
+        
+        var_dump($crawler);exit;
+
+        $response = $client->getResponse();
+
+        $content = $response->getContent();
+        
+        // Load a file 
+        $html->load_file('http://www.spanishdict.com/conjugate/comer'); 
+        
+        $firstTable = $html->find('.table', 0);
+        
+        var_dump($firstTable);exit;
         
     }
 }
